99f49d6a9d14fe24a7614d11af4825171d442e85
계주성 <kyejusung@gmail.com>
2025-05-07 17:36:13 +0900

    박현준님 취미 MBTI 수정

--- BestiOSDevelopers/SelfIntroductionData.swift
+++ BestiOSDevelopers/SelfIntroductionData.swift
@@ -242,9 +242,9 @@ let people: [SelfIntroduction] = [
     SelfIntroduction(
         name: "박현준",
         trainingPurpose: "iOS 개발자가 되기 위한 훈련 참여",
-        hobby: "음악 감상, 여행, 독서 등",
-        mbti: "INTJ",
-        strength: "끈기와 실행력",
+        hobby: "스카이 다이빙",
+        mbti: "INFP",
+        strength: "머리가 좋다.",
         sns: nil,
         etc: "열심히 수료하고 취업하겠습니다!",
         job: "카카오 - iOS 개발자"
